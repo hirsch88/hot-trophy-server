31e04f3775e7c7b6520d40abf2f0aee31d1fab89
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2015-02-25 12:07:13 +0100

    unit tests

--- app/api/services/UtilService.js
+++ app/api/services/UtilService.js
@@ -3,16 +3,24 @@ var Promise = require('bluebird');
 module.exports = {
 
     hasRequiredAttributes: function (requiredAttrs, body) {
+        if(requiredAttrs === undefined || requiredAttrs === null){
+            return true;
+        }
+
+        if(requiredAttrs.length > 0 && (body === undefined || body === null)){
+            return false;
+        }
+
         var val;
         for (var i = 0; i < requiredAttrs.length; i++) {
             val = body[requiredAttrs[i]];
-            if( val === null || val === undefined ){
+            if (val === null || val === undefined || val === '') {
                 return false;
             }
         }
         return true;
-    }
 
+    }
 
 
 };
